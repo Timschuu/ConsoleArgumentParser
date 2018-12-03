c5f9790cffff1253b16399c34cb6d8c0e5cfaa2a
Timschuu <timschu3@gmail.com>
2018-12-04 00:34:03 +0100

    add changes from assembler repo

---
+++ ConsoleArgumentParserTestProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using ConsoleArgumentParser;
+
+namespace ConsoleArgumentParserTestProgram
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parser parser = new Parser("-", "--");
+            parser.RegisterCommand(new CommandType(typeof(TestCommand), "this is a test command")); 
+
+
+            parser.ParseCommand(args[0], args.ToList().GetRange(1, args.Length - 1));
+        }
+    }
+}
---
+++ ConsoleArgumentParserTestProgram/TestCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleArgumentParser;
+using ICommand = ConsoleArgumentParser.ICommand;
+
+namespace ConsoleArgumentParserTestProgram
+{
+    [Command("-w")]
+    public class TestCommand : ICommand
+    {
+        private readonly List<string> _messages;
+        private bool _red;
+        
+        public void Execute()
+        {
+            if (_red)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            foreach (string message in _messages)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        public TestCommand(IEnumerable<string> args)
+        {
+            _messages = new List<string>();
+            _messages.AddRange(args);
+        }
+
+        [CommandArgument("--r")]
+        private void RedSubCommand(IEnumerable<string> args)
+        {
+            _red = true;
+        }
+    }
+}
